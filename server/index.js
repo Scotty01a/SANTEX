93de4eff36da13698d046212307eb3fd098a8cb5
Scotty01a <jaedenscott6@gmail.com>
2023-07-17 15:52:47 +0100

    Database connected

--- server/index.js
+++ server/index.js
@@ -20,3 +20,71 @@ app.listen(port, () => {
 })
 
 app.get('/', (req, res) => console.log(res.send("Welcome to our web server")))
+
+// a. GET /expenses to retrieve all data from the db
+
+app.get('/expenses', async (req, res) => {
+    let data = await expenseModel.find()
+    res.send(data)
+})
+
+// b. POST /expenses to post an expense
+
+app.post('/expenses', async (req, res) => {
+    const newExpense = new expenseModel({
+        name: "Transport fee",
+        amount: 1000,
+        date: "April 1st",
+        invoice: "Transport Payment"
+
+    }
+    )
+
+    const data = await newExpense.save()
+    res.send(data) 
+})
+
+// c PUT /expenses/:id to update an id
+
+app.put('/expenses/:id', async (req, res) => {
+    const id = req.params.id
+
+    const newExpense = new expenseModel({
+        name: "Updated Transport",
+        amount: 1000,
+        date: "April 1st",
+        invoice: "Transport Payment"
+    }
+    )
+
+
+    let data = await expenseModel.findByIdAndUpdate(id, {$set: newExpense}, {new:true})
+
+    res.send(data)
+})
+
+
+// d DELETE /expenses/:id to delete an id
+
+app.delete('/expenses/:id', async (req, res) => {
+    const id = req.params.id
+
+   
+
+    let data = await expenseModel.findByIdAndDelete(id)
+
+    res.send(data)
+
+})
+
+// d Get /expenses/:id to get an id
+
+app.get('/expenses/:id', async (req, res) => {
+    const id = req.params.id
+
+   
+
+    let data = await expenseModel.findById(id)
+
+    res.send(data)
+})
